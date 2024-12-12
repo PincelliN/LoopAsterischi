fe775d1e080feea955d57943a9c8200e9af0b50d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-12 12:39:34 +0100

    infinite loop while

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* 
+implementare un ciclo infinito 
+-acquisisci dal terminale  un input
+-se l'input  e uguale a "no" esci dal loop
+-visualizza una serie di n righe  e per ogni riga un numero crescente di * 
+ */
+int loop;
+
+while (true)
+{
+
+}
+
